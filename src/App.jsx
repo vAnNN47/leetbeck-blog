40b4c5d1f0db9521a956e5de1daf971e959bdaff
vANN47 <leetbeck@gmail.com>
2024-06-10 22:01:04 +0300

    fixed mobile dimension

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import './App.css'
 import { Portfolio } from './components/Portfolio'
+
 function App() {
 
   return (
-    <>
-      <div>
-        <Portfolio />
-      </div>
-    </>
+    <Portfolio />
   )
 }
 
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,22 +1,37 @@
+import styled from "styled-components"
+
+const ResponsiveImage = styled.img`
+    width: 100%;
+    max-width: 600px;
+    height: auto;
+    border: 1px solid black;
+
+    @media(min-width: 768px){
+        width: 80%
+    }
+
+    @media (min-width: 1024px){
+        width: 60%;
+    }
+`
+
 export const Portfolio = () => {
     return (
         <div>
             <div>
                 <h2>Leetbeck Portfolio</h2>
-                <img
+                <ResponsiveImage
                     src="/images/fibo.webp"
                     alt="fibo art style"
-                    width="450"
                 />
                 <p>Trying some fibo style art</p>
                 <hr />
             </div>
             <div>
                 <h2>Leetbeck Portfolio</h2>
-                <img
+                <ResponsiveImage
                     src="/images/t-shirt-style.webp"
                     alt="fibo art style"
-                    width="450"
                 />
                 <p>Some t-shirt style idea</p>
                 <hr />
