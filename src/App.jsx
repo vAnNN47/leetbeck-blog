c7827ca24996e9a65f002343a44bb56a788493d9
vANN47 <leetbeck@gmail.com>
2024-06-10 22:31:25 +0300

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import './App.css'
-import { Portfolio } from './components/Portfolio'
+import './App.css';
+import { Portfolio } from './components/Portfolio';
+import { Loader } from './components/Loader';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false)
+    }, 2000)
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <Loader />
+  }
 
   return (
     <Portfolio />
   )
 }
 
-export default App
+export default App;
+
---
+++ src/components/Loader.jsx
@@ -0,0 +1,10 @@
+import './Loader.css';
+
+export const Loader = () => {
+    return (
+        <div className='loader'>
+            <div className='spinner'></div>
+            <p>Loading...</p>
+        </div>
+    )
+}
