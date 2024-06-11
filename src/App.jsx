0ec9a123320623a6c68f4aec6c24ce9b5d04979a
vANN47 <leetbeck@gmail.com>
2024-06-11 09:24:54 +0300

    getting ready to inspect all comit to fix loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import './App.css';
 import { Portfolio } from './components/Portfolio';
 import { Loader } from './components/Loader';
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 
-function App() {
+// c7827ca24996e9a65f002343a44bb56a788493d9
+
+const App = () => {
+
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    // Set up an event listener for the window load event
     const handleLoad = () => {
-      console.log("Page fully loaded");
       setIsLoading(false);
     };
 
     window.addEventListener('load', handleLoad);
 
-    // Cleanup event listener
-    return () => window.removeEventListener('load', handleLoad);
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('load', handleLoad);
+    };
   }, []);
-
-  console.log("isLoading:", isLoading);
 
   if (isLoading) {
     return <Loader />;
@@ -26,7 +29,8 @@ function App() {
 
   return (
     <Portfolio />
-  );
+  )
 }
 
 export default App;
+
