148924150251dcfe4977f949f11cea01616e18ef
vANN47 <leetbeck@gmail.com>
2024-06-11 09:11:20 +0300

    fixed loader?

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,29 @@ import { Loader } from './components/Loader';
 import { useEffect, useState } from 'react';
 
 function App() {
-
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-
     const handleLoad = () => {
+      console.log("Page fully loaded");
       setIsLoading(false);
-    }
+    };
 
     window.addEventListener('load', handleLoad);
 
+    // Cleanup event listener
     return () => window.removeEventListener('load', handleLoad);
   }, []);
 
+  console.log("isLoading:", isLoading);
+
   if (isLoading) {
-    return <Loader />
+    return <Loader />;
   }
 
   return (
     <Portfolio />
-  )
+  );
 }
 
 export default App;
-
