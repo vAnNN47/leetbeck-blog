2a2cbfcca8d903c891f6ed958e8342d66f1f0ba6
unknown <leetbeck@gmail.com>
2024-06-16 19:05:14 +0300

    ok testing loader again

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import './App.css';
-import { Portfolio } from './components/Portfolio';
-// import { Loader } from './components/Loader';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Portfolio } from "./components/Portfolio";
+import { Loader } from "./components/Loader";
 
 function App() {
-  return (
-    <Portfolio />
-  )
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Set the isLoading state to false when the window is fully loaded
+    const handleLoad = () => {
+      setIsLoading(false);
+    };
+
+    window.addEventListener("load", handleLoad);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("load", handleLoad);
+    };
+  }, []);
+
+  return <div className="App">{isLoading ? <Loader /> : <Portfolio />}</div>;
 }
 
 export default App;
-
