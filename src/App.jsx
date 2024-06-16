2339622bcd1a4a42588ab483cecafa4ad81b6805
vANN47 <leetbeck@gmail.com>
2024-06-16 20:45:48 +0300

    changed to image loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import { Portfolio } from "./components/Portfolio";
 import { Loader } from "./components/Loader";
@@ -6,10 +6,13 @@ import { useImagesLoaded } from "./components/useImagesLoaded";
 
 function App() {
   const imagePaths = ["/images/fibo.webp", "/images/t-shirt-style.webp"];
-
   const imagesLoaded = useImagesLoaded(imagePaths);
 
-  return <div className="App">{imagesLoaded ? <Portfolio /> : <Loader />}</div>;
+  return (
+    <div className="App">
+      {imagesLoaded ? <Portfolio /> : <Loader />}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,9 +2,8 @@ import './Loader.css';
 
 export const Loader = () => {
     return (
-        <div className='loader'>
-            <div className='spinner'></div>
-            <p>Loading...</p>
+        <div className="loader">
+            <img className='spinner' src="/images/loader.webp" alt="Loading..." />
         </div>
-    )
-}
+    );
+};
